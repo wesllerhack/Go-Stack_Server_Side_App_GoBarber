847e6a29355f2b3c8c15444ff73f412d7a044558
Wesller Hack <wesllerhack@hotmail.com>
2022-11-16 05:22:40 -0300

    saving password revome with class-transformer

--- src/modules/users/infra/http/controller/ProfileController.ts
+++ src/modules/users/infra/http/controller/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfile from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -32,6 +33,6 @@ export default class ProfileController {
 
         //delete user.password;
 
-        return response.json(user);
+        return response.json(classToClass(user));
     }
 }
--- src/modules/users/infra/typeorm/entities/Users.ts
+++ src/modules/users/infra/typeorm/entities/Users.ts
@@ -1,5 +1,7 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
+import { Exclude, Expose } from 'class-transformer';
+
 @Entity('users')
 class User {
   @PrimaryGeneratedColumn('uuid')
@@ -12,6 +14,7 @@ class User {
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @Column()
@@ -23,6 +26,10 @@ class User {
   @UpdateDateColumn()
   updated_at: Date;
 
+  @Expose({name: 'avatar_url' })
+  getAvat_url(): string | null{
+    return this.avatar ? `${process.env.APP_API_URL}/files/${this.avatar}` : null;
+  }
 }
 
 export default User;
